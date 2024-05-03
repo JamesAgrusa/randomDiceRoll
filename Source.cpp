c97859be08761f6ee2168f38ef0df1920f60f835
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-05-02 22:22:39 -0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+void roll();
+char rollAgain();
+
+int main()
+{
+	roll();
+	rollAgain();
+}
+
+void roll()
+{
+	string diceSide;
+	string side[] = { "one", "two", "three", "four", "five", "six" };
+	srand((unsigned int)time(NULL));
+	diceSide = side[rand() % 6];
+	cout << "dice rolled: " << diceSide << endl;
+}
+
+char rollAgain()
+{
+	char choice;
+	cout << "Would you like to roll again?" << endl;
+	cout << "Yes(Y) or No(N)" << endl;
+	cin >> choice;
+	if (choice == 'Y')
+	{
+		string diceSide;
+		string side[] = { "one", "two", "three", "four", "five", "six" };
+		srand((unsigned int)time(NULL));
+		diceSide = side[rand() % 6];
+		cout << "dice rolled: " << diceSide << endl;
+	}
+	else
+	{
+		cout << "Thanks for using me!" << endl;
+	}
+	do
+	{
+		rollAgain();
+	} while (choice == 'Y');
+
+	return choice;
+}
